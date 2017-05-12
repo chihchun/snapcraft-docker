44e7acf5d6cb02b29312ec89b5cb079e28834620
Rex Tsai (蔡志展) <rex.tsai@canonical.com>
2017-05-12 21:21:14 +0800

    Add image based on zesty 17.04.

--- baseimage/build-scrtch.sh
+++ baseimage/build-scrtch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Scripts to build the base images from scratch.
 
-for VERSION in 16.04.2 ; do
+for VERSION in 16.04.2 17.04; do
     for ARCH in amd64 i386 armhf arm64 ; do
         curl http://cdimage.ubuntu.com/ubuntu-base/releases/$VERSION/release/ubuntu-base-$VERSION-base-$ARCH.tar.gz | docker import - snapcraft/ubuntu-base:$ARCH-$VERSION
         docker push snapcraft/ubuntu-base:$ARCH-$VERSION
