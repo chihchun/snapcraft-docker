678ad633930686dec8a5d48149b1547f9ef25701
Heissler Jérôme <heissler.jerome@gmail.com>
2017-08-18 10:48:03 +0200

    create option parameters to force apt-get update

--- xenial/armhf/2.32/docker-entrypoint.sh
+++ xenial/armhf/2.32/docker-entrypoint.sh
@@ -2,10 +2,25 @@
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
+OPTIND=1
+
+# Initialize variables:
+force_update=0
+while getopts "u?" opt; do
+    case "$opt" in
+    u)  force_update=1
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "$1" = "--" ] && shift
+
 run_snapcraft () {
     LAST=$(($(date +%s) - $(date +%s -r /var/cache/apt/archives/lock)))
     # update APT if the image is older than one month
-    if [ $LAST -qe 2592000 ] ; then
+    if [ $force_update == 1 ] || [ $LAST -ge 2592000 ] ; then
         sudo apt-get update
     fi
     exec snapcraft $@
