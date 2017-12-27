08afe796fffd7cd453676a1fe433c984c5341f77
Rex Tsai (蔡志展) <rex.tsai@canonical.com>
2017-12-27 21:36:08 +0800

    Add 2.35, 2.36 for amd64

---
+++ xenial/amd64/2.36/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+run_snapcraft () {
+    LAST=$(($(date +%s) - $(date +%s -r /var/cache/apt/archives/lock)))
+
+    # update APT if the image is older than one month
+    if [[ "${LAST}" > "2592000" ]] ; then 
+        sudo apt-get update
+    fi
+    exec snapcraft $@
+}
+
+if [ -z $1 ] ; then
+    run_snapcraft
+else
+    if [ $1 != "init" ] && \
+    [ $1 != "snap" ] && \
+    [ $1 != "login" ] && \
+    [ $1 != "logout" ] && \
+    [ $(which -- $1) ] ; then
+        exec $@
+    else
+        run_snapcraft $@
+    fi
+fi
